215fba0b4d95f87d59fc5da08a7a570d4047e1c4
thomasmmc <thomasmmc@gmail.com>
2014-10-23 21:07:21 -0400

    initial commit

---
+++ splunk_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+version=6.1.4-233537
+ echo -e "What version of splunk do you want to install? default($version):"
+ read u_version
+ if [ $u_version ]
+   then
+   ver_split=${version//-/$'\n'};
+   build=${ver_split[0]};
+   version=${ver_split[1]};
+ else
+    ver_split=${u_version//-/$'\n'};
+    build=${ver_split[0]};
+    version=${ver_split[1]};
+ fi
+
+wget=/usr/bin/wget
+WGET_OPTS="-O splunk-$ver-$build-Linux-x86_64.tgz"
+
+ $wget ${WGET_OPTS} "http://www.splunk.com/page/download_track?file=$ver/splunk/linux/splunk-$ver-$build-Linux-x86_64.tgz&ac=&wget=true&name=wget&platform=Linux&architecture=x86_64&version=$ver&product=splunk&typed=release"
