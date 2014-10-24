9c978a9d2b32f4f88b66738066ddb84e79fc5258
thomasmmc <thomasmmc@gmail.com>
2014-10-23 21:52:38 -0400

    reordered if statement

--- splunk_install.sh
+++ splunk_install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 version=6.1.4-233537
- echo -e "What version of splunk do you want to install? default($version):"
+ echo "What version-build of splunk should we install? default($version):"
  read u_version
  if [ $u_version ]
    then
-   ver_split=${version//-/$'\n'};
-   build=${ver_split[0]};
-   version=${ver_split[1]};
- else
-    ver_split=${u_version//-/$'\n'};
-    build=${ver_split[0]};
-    version=${ver_split[1]};
+    ver_split=${u_version/-/ };
+    version=${ver_split[0]};
+    build=${ver_split[1]};
+  else
+    ver_split=(${version/-/ })
+    version=${ver_split[0]};
+    build=${ver_split[1]};
  fi
 
 wget=/usr/bin/wget
 WGET_OPTS="-O splunk-$ver-$build-Linux-x86_64.tgz"
 
- $wget ${WGET_OPTS} "http://www.splunk.com/page/download_track?file=$version/splunk/linux/splunk-$version-$build-Linux-x86_64.tgz&ac=&wget=true&name=wget&platform=Linux&architecture=x86_64&version=$version&product=splunk&typed=release"
+$wget ${WGET_OPTS}  "http://www.splunk.com/page/download_track?file=$version/splunk/linux/splunk-$version-$build-Linux-x86_64.tgz&ac=&wget=true&name=wget&platform=Linux&architecture=x86_64&version=$version&product=splunk&typed=release"
