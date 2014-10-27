e9e7e57f7e9569a9fe5b89c8b72e478eb83c9970
thomasmmc <thomasmmc@gmail.com>
2014-10-27 10:30:30 -0400

    fixed broken variable

--- splunk_install.sh
+++ splunk_install.sh
@@ -20,11 +20,11 @@ fi
 
 
 wget=/usr/bin/wget
-WGET_OPTS="-O splunk-$ver-$build-Linux-x86_64.tgz"
+WGET_OPTS="-O splunk-$version-$build-Linux-x86_64.tgz"
 
 $wget ${WGET_OPTS} "http://www.splunk.com/page/download_track?file=$version/splunk/linux/splunk-$version-$build-Linux-x86_64.tgz&ac=&wget=true&name=wget&platform=Linux&architecture=x86_64&version=$version&product=splunk&typed=release"
 
-tar -xzvf splunk-6.1.4-233537-Linux-x86_64.tgz  --directory=$SPLUNK_HOME --strip-components=1
+tar -xzvf splunk-$version-$build-Linux-x86_64.tgz  --directory=$SPLUNK_HOME --strip-components=1
 
 
 splunk start --accept-license --answer-yes --auto-ports --no-prompt
